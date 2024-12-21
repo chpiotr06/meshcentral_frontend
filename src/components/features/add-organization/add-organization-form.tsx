c91128acbd141b996bd57a66fcff2674dceb0a6c
kpekala <kgpekala@gmail.com>
2024-12-21 20:14:53 +0100

    feat: style organization form

--- src/components/features/add-organization/add-organization-form.tsx
+++ src/components/features/add-organization/add-organization-form.tsx
@@ -39,98 +39,106 @@ export const AddOrganizationForm = () => {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Company Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Acme inc." {...field} />
-              </FormControl>
-              <FormDescription>Legal name of the company.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="country"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Country</FormLabel>
-              <FormControl>
-                <Input placeholder="Poland" {...field} />
-              </FormControl>
-              <FormDescription>
-                Legal country of registration of the company.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="postal"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Postal code</FormLabel>
-              <FormControl>
-                <Input placeholder="31-122" {...field} />
-              </FormControl>
-              <FormDescription>Postal code of company</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="addressLine1"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address Line 1</FormLabel>
-              <FormControl>
-                <Input placeholder="Cracow" {...field} />
-              </FormControl>
-              <FormDescription>First line of the address.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="addressLine2"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address Line 2</FormLabel>
-              <FormControl>
-                <Input placeholder="NY Avenue 42" {...field} />
-              </FormControl>
-              <FormDescription>Second line of the address.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="addressLine3"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address Line 3</FormLabel>
-              <FormControl>
-                <Input placeholder="Apartment 13" {...field} />
-              </FormControl>
-              <FormDescription>Third line of the address.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" disabled={isSuccess || isPending}>
-          {!isPending ? "Submit" : <LoaderCircle className="animate-spin" />}
-        </Button>
-      </form>
-    </Form>
+    <div className="border grow-0 p-4 rounded-md mt-8 place-self-center">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="flex gap-8">
+            <div className="w-80">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Acme inc." {...field} />
+                    </FormControl>
+                    <FormDescription>Legal name of the company.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="country"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Poland" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Legal country of registration of the company.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="postal"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Postal code</FormLabel>
+                    <FormControl>
+                      <Input placeholder="31-122" {...field} />
+                    </FormControl>
+                    <FormDescription>Postal code of company</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+          </div>
+          <div className="w-64">
+            <FormField
+                control={form.control}
+                name="addressLine1"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Address Line 1</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Cracow" {...field} />
+                    </FormControl>
+                    <FormDescription>First line of the address.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="addressLine2"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Address Line 2</FormLabel>
+                    <FormControl>
+                      <Input placeholder="NY Avenue 42" {...field} />
+                    </FormControl>
+                    <FormDescription>Second line of the address.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="addressLine3"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Address Line 3</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Apartment 13" {...field} />
+                    </FormControl>
+                    <FormDescription>Third line of the address.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={isSuccess || isPending} style={{float: "right", marginTop: 8}}>
+                  {!isPending ? "Submit" : <LoaderCircle className="animate-spin" />}
+              </Button>
+            </div> 
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 };
